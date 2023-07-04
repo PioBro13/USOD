c8ac723dbef2fcf5e3224408e13d64507e922a49
piotr <piotr.bro33@gmail.com>
2023-07-04 22:19:40 +0200

    Initial Commit

---
+++ src/main/java/pl/usod/UsodApplication.java
@@ -0,0 +1,13 @@
+package pl.usod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/usod/UsodApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.usod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
