911c74ed032b5b6f08e4cf0cc2003e2ca37255eb
pbrodka13 <pbrodka@opi.org.pl>
2023-07-29 16:54:04 +0200

    remaining fields for term results

--- src/main/java/pl/usod/model/TermResults.java
+++ src/main/java/pl/usod/model/TermResults.java
@@ -12,6 +12,7 @@ public class TermResults {
     @Column(name = "termId")
     private int termNumber;
 
+    //TODO link with students table
     @Column(name = "studentId")
     protected Long studentId;
 
@@ -24,7 +25,20 @@ public class TermResults {
     @Column(name = "studentGroup")
     private String studentGroup;
 
-    @Column(name="")
+    @Column(name="startingECTS")
+    private int startingECTS;
+
+    @Column(name = "finalECTS")
+    private int finalECTS;
+
+    @Column(name="nominalECTS")
+    private int nominalECTS;
+
+    @Column(name = "obtainedECTS")
+    private int obtainedECTS;
+
+    @Column(name = "speciality")
+    private String speciality;
 
 
 
@@ -34,5 +48,85 @@ public class TermResults {
 
     public Long getId() {
         return id;
+    }
+
+    public int getTermNumber() {
+        return termNumber;
+    }
+
+    public void setTermNumber(int termNumber) {
+        this.termNumber = termNumber;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getStudentTermNumber() {
+        return studentTermNumber;
+    }
+
+    public void setStudentTermNumber(int studentTermNumber) {
+        this.studentTermNumber = studentTermNumber;
+    }
+
+    public String getRegisterType() {
+        return registerType;
+    }
+
+    public void setRegisterType(String registerType) {
+        this.registerType = registerType;
+    }
+
+    public String getStudentGroup() {
+        return studentGroup;
+    }
+
+    public void setStudentGroup(String studentGroup) {
+        this.studentGroup = studentGroup;
+    }
+
+    public int getStartingECTS() {
+        return startingECTS;
+    }
+
+    public void setStartingECTS(int startingECTS) {
+        this.startingECTS = startingECTS;
+    }
+
+    public int getFinalECTS() {
+        return finalECTS;
+    }
+
+    public void setFinalECTS(int finalECTS) {
+        this.finalECTS = finalECTS;
+    }
+
+    public int getNominalECTS() {
+        return nominalECTS;
+    }
+
+    public void setNominalECTS(int nominalECTS) {
+        this.nominalECTS = nominalECTS;
+    }
+
+    public int getObtainedECTS() {
+        return obtainedECTS;
+    }
+
+    public void setObtainedECTS(int obtainedECTS) {
+        this.obtainedECTS = obtainedECTS;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
     }
 }
