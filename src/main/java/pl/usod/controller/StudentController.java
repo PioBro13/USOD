b4e78bca4bd9b305ad2ca08d1cdc9beb4c901a63
piotr <piotr.bro33@gmail.com>
2023-08-01 21:57:39 +0200

    simple get one mapping apis

--- src/main/java/pl/usod/controller/StudentController.java
+++ src/main/java/pl/usod/controller/StudentController.java
@@ -2,12 +2,14 @@ package pl.usod.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.usod.model.Student;
 import pl.usod.repository.StudentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/students")
@@ -18,6 +20,12 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getOne(@PathVariable Long id){
+        return studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("no student with given id"));
     }
 
 }
--- src/main/java/pl/usod/controller/TermResultsController.java
+++ src/main/java/pl/usod/controller/TermResultsController.java
@@ -2,8 +2,10 @@ package pl.usod.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.usod.model.Student;
 import pl.usod.model.TermResults;
 import pl.usod.repository.TermResultsRepository;
 
@@ -18,6 +20,12 @@ public class TermResultsController {
     @GetMapping
     public List<TermResults> getTermResults(){
         return termResultsRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TermResults getOne(@PathVariable Long id){
+        return termResultsRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("There are no term results with given id"));
     }
 
 }
