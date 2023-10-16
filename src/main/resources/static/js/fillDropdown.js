01db46a081cc072feed4c1720ac32b81512f4303
pbrodka13 <pbrodka@opi.org.pl>
2023-10-16 17:41:11 +0200

    fulfill dropdown with data from API (only for operation finances)

--- src/main/resources/static/js/fillDropdown.js
+++ src/main/resources/static/js/fillDropdown.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'http://localhost:8080/api' + document.getElementById('').getAttribute('endpoint');
-    fetch('http://example.com/api/data') // zastąp 'http://example.com/api/data' adresem Twojego endpointu
-        .then(response => response.json())
+    const dropdownMenu = document.getElementById('myDropdown');
+    fetch('http://localhost:8080/api/operationFinances')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
-            const dropdown = document.getElementById('myDropdown');
-            data.forEach(item => {
-                const option = document.createElement('option');
-                option.text = item.id;
-                option.value = item.id;
-                dropdown.add(option);
-            });
+            console.log("Received data:", data);
+            const dropdownMenu = document.getElementById('myDropdown');
+            if (Array.isArray(data)) {
+                data.forEach(item => {
+                    const listItem = document.createElement('li');
+                    listItem.classList.add('dropdown-item');
+                    listItem.textContent = item.id + ' ' +item.operationName;
+                    dropdownMenu.appendChild(listItem);
+                });
+            } else {
+                console.error("Invalid data format");
+            }
         })
         .catch(error => {
-            console.error('Error fetching data:', error);
+            console.error('Error:', error);
         });
-
-
-
 });
