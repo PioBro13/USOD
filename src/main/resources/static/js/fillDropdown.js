68365366afee332178f3b0c72055d9f80718b920
pbrodka13 <pbrodka@opi.org.pl>
2023-10-16 18:02:48 +0200

    prepare for universal use of fill dropdown function

--- src/main/resources/static/js/fillDropdown.js
+++ src/main/resources/static/js/fillDropdown.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const dropdownMenu = document.getElementById('myDropdown');
+    const dropdownButton = document.getElementById('dropdownMenu')
+    const apiUrl = 'http://localhost:8080/api' + dropdownButton.getAttribute('endpoint');
     fetch('http://localhost:8080/api/operationFinances')
         .then(response => {
             if (!response.ok) {
@@ -15,6 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     const listItem = document.createElement('li');
                     listItem.classList.add('dropdown-item');
                     listItem.textContent = item.id + ' ' +item.operationName;
+                    listItem.setAttribute('value', item.id); // Ustawiamy id jako wartość atrybutu value
+                    listItem.addEventListener('click', function() {
+                        console.log("Selected ID:", item.id); // Wyświetlamy id wybranego elementu w konsoli
+                        dropdownButton.textContent =  item.id + ' ' +item.operationName;
+                        dropdownButton.setAttribute('selected-value' , item.id);
+                    });
                     dropdownMenu.appendChild(listItem);
                 });
             } else {
