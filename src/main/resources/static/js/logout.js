8aa7fd82536f820a04763af8e455447ccd0ed4eb
pbrodka13 <pbrodka@opi.org.pl>
2023-12-28 14:22:46 +0100

    Login and logout buttons refactor, and fully working as intended

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -2,11 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('logout').addEventListener('click', function (event) {
         const content = document.getElementById('content');
         const adminContent = document.getElementById('content-admin');
+        const logoutButton = document.getElementById('logout');
+        const loginButton = document.getElementById('login');
 
-        content.classList.add("hide");
+        if(content){
+            content.classList.add("hide");
+        }
+
         if(adminContent){
             adminContent.classList.add("hide");
         }
+        if(loginButton){
+            logoutButton.classList.add("hide");
+            loginButton.classList.remove("hide");
+        }
+
         localStorage.clear();
         window.location.href = '/';
     });
