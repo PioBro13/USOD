7cb0964c87d325f7c0a2fb8ab4b00b2b296798e6
pbrodka13 <pbrodka@opi.org.pl>
2024-02-14 16:36:19 +0100

    New redirect financial summary form and refactor of html files

--- src/main/resources/static/js/orbeonRedirect.js
+++ src/main/resources/static/js/orbeonRedirect.js
@@ -2,12 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const orbeonBaseLink = "http://localhost:8040/orbeon/fr";
     const userId = '?id=' + localStorage.getItem('userId');
     const resultsSummary = document.getElementById('results-overall-summary');
+    const financesSummary = document.getElementById('overall-finances-summary');
     if(resultsSummary){
         resultsSummary.addEventListener('click', function () {
             window.location.href= orbeonBaseLink + '/summary/student_results_summary/new' +  userId;
         });
     }
-
+    if(financesSummary){
+        financesSummary.addEventListener('click', function () {
+            window.location.href= orbeonBaseLink + '/summary/student_financial_summary/new' +  userId;
+        });
+    }
 
     }
 );
